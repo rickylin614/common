8aa3cc8289e3be55259d50303562f29106772256
rickylin614 <syss8895@gmail.com>
2023-12-03 05:03:51 +0800

    feat: mongo tool

--- cmongo/interface.go
+++ cmongo/interface.go
@@ -1,27 +1,21 @@
 package cmongo
 
-import (
-	"context"
+// type MongoDBWrapper interface {
+// 	Connect(ctx context.Context, uri string, database string) error
 
-	"go.mongodb.org/mongo-driver/bson"
-)
+// 	// CRUD operations
+// 	Insert(ctx context.Context, collection string, document interface{}) error
+// 	Find(ctx context.Context, collection string, target any) error
+// 	Update(ctx context.Context, collection string, query interface{}, update interface{}) error
+// 	Delete(ctx context.Context, collection string, query interface{}) error
+// 	// UpdateBatch(collection string, updates []mongo.WriteModel)
+// 	// DeleteBatch(collection string, deletes []mongo.WriteModel) error
 
-type MongoDBWrapper interface {
-	Connect(uri string) error
-
-	// CRUD operations
-	Insert(ctx context.Context, collection string, document interface{}) error
-	Find(ctx context.Context, collection string, query interface{}) ([]bson.M, error)
-	Update(ctx context.Context, collection string, query interface{}, update interface{}) error
-	Delete(ctx context.Context, collection string, query interface{}) error
-	// UpdateBatch(collection string, updates []mongo.WriteModel)
-	// DeleteBatch(collection string, deletes []mongo.WriteModel) error
-
-	// Fluent interface methods
-	Where(field string, comparison string, value interface{}) MongoDBWrapper
-	Sort(field string, order string) MongoDBWrapper
-	Limit(limit int64) MongoDBWrapper
-	Offset(offset int64) MongoDBWrapper
-	GroupBy(field string) MongoDBWrapper
-	Having(condition bson.M) MongoDBWrapper
-}
+// 	// Fluent interface methods
+// 	Where(field string, value interface{}, comparison ...string) MongoDBWrapper
+// 	Sort(field string, order string) MongoDBWrapper
+// 	Limit(limit int64) MongoDBWrapper
+// 	Offset(offset int64) MongoDBWrapper
+// 	GroupBy(field string) MongoDBWrapper
+// 	Having(condition bson.M) MongoDBWrapper
+// }
--- cmongo/model.go
+++ cmongo/model.go
@@ -2,134 +2,225 @@ package cmongo
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-type mongoDBWrapper struct {
-	client     *mongo.Client
-	collection *mongo.Collection
-	database   string
-
-	queryFilter bson.D
-	sort        bson.D
-	limit       int64
-	offset      int64
-	groupBy     bson.D
+type MongoDB struct {
+	client   *mongo.Client
+	database string
 }
 
-func (m *mongoDBWrapper) Clone() MongoDBWrapper {
-	cloned := *m // 淺拷貝
-	// 如果有引用類型的字段，需要進行深拷貝
-	// 例如，如果 queryFilter 是引用類型，則需要：
-	if m.queryFilter != nil {
-		cloned.queryFilter = make(bson.D, len(m.queryFilter))
-		copy(cloned.queryFilter, m.queryFilter)
+func NewMongoDB() *MongoDB {
+	return &MongoDB{}
+}
+
+func (m *MongoDB) Connect(ctx context.Context, uri, database string) (*MongoDB, error) {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+	if err != nil {
+		return nil, err
 	}
-	// 對其他可能的引用類型字段進行類似操作...
-	return &cloned
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, err
+	}
+
+	// 獲取數據庫列表
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	// 檢查指定的數據庫是否存在
+	dbExists := false
+	for _, db := range databases {
+		if db == database {
+			dbExists = true
+			break
+		}
+	}
+
+	if !dbExists {
+		return nil, fmt.Errorf("database %s does not exist", database)
+	}
+
+	m.client = client
+	m.database = database
+
+	return m, nil
 }
 
-func (m *mongoDBWrapper) Connect(uri string) error {
+func (m *MongoDB) Insert(ctx context.Context, collection string, document interface{}) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m *mongoDBWrapper) Insert(ctx context.Context, collection string, document interface{}) error {
+func (m *MongoDB) Update(ctx context.Context, collection string, query interface{}, update interface{}) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m *mongoDBWrapper) Find(ctx context.Context, collection string, query interface{}) ([]bson.M, error) {
-	// 設置集合
-	m.collection = m.client.Database(m.database).Collection(collection)
+func (m *MongoDB) UpdateBatch(ctx context.Context, collection string, updates []mongo.WriteModel) error {
+	c := m.client.Database(m.database).Collection(collection)
+	_, err := c.BulkWrite(ctx, updates)
+	return err
+}
 
-	// 轉換自定義查詢條件為BSON
-	var bsonQuery bson.M
-	if query != nil {
-		bsonBytes, err := bson.Marshal(query)
-		if err != nil {
-			return nil, err
-		}
-		err = bson.Unmarshal(bsonBytes, &bsonQuery)
-		if err != nil {
-			return nil, err
-		}
-	}
+func (m *MongoDB) Delete(ctx context.Context, collection string, query interface{}) error {
+	//TODO implement me
+	panic("implement me")
+}
 
-	// 構建查詢過濾條件
-	var finalFilter bson.D
-	if len(m.queryFilter) > 0 {
-		finalFilter = append(finalFilter, bson.E{Key: "$and", Value: bson.A{m.queryFilter, bsonQuery}})
+func (m *MongoDB) DeleteBatch(ctx context.Context, collection string, deletes []mongo.WriteModel) error {
+	c := m.client.Database(m.database).Collection(collection)
+	_, err := c.BulkWrite(ctx, deletes)
+	return err
+}
+
+func (m *MongoDB) Find(ctx context.Context, table string, qb *queryBuilder, results any) error {
+	filter, group, opts := qb.Build()
+	collection := m.client.Database(m.database).Collection(table)
+	if len(group) > 0 {
+		pipeline := mongo.Pipeline{
+			{{"$match", filter}},
+			{{"$group", group}},
+		}
+		if len(qb.having) > 0 {
+			pipeline = append(pipeline, bson.D{{"$match", qb.having}})
+		}
+		cursor, err := collection.Aggregate(ctx, pipeline)
+		if err != nil {
+			return err
+		}
+		defer cursor.Close(ctx)
+		return cursor.All(ctx, results)
 	} else {
-		finalFilter = m.queryFilter
+		cursor, err := collection.Find(ctx, filter, opts)
+		if err != nil {
+			return err
+		}
+		defer cursor.Close(ctx)
+		return cursor.All(ctx, results)
 	}
+}
 
-	// 設置查詢選項
+func (m *MongoDB) Count(ctx context.Context, table string, qb *queryBuilder) (int64, error) {
+	filter, group, _ := qb.Build()
+	collection := m.client.Database(m.database).Collection(table)
+	if len(group) > 0 {
+		pipeline := mongo.Pipeline{
+			{{"$match", filter}},
+			{{"$group", group}},
+		}
+		if len(qb.having) > 0 {
+			pipeline = append(pipeline, bson.D{{"$match", qb.having}})
+		}
+		pipeline = append(pipeline, bson.D{{"$count", "count"}})
+
+		cursor, err := collection.Aggregate(ctx, pipeline)
+		if err != nil {
+			return 0, err
+		}
+		defer cursor.Close(ctx)
+		var results []bson.M
+		if err := cursor.All(ctx, &results); err != nil {
+			return 0, err
+		}
+		if len(results) > 0 {
+			return results[0]["count"].(int64), nil
+		}
+		return 0, nil
+	} else {
+		count, err := collection.CountDocuments(ctx, filter)
+		if err != nil {
+			return 0, err
+		}
+		return count, nil
+	}
+}
+
+type queryBuilder struct {
+	filter    bson.D
+	sort      bson.D
+	limit     int64
+	offset    int64
+	group     bson.D
+	sumFields map[string]bool
+	having    bson.D
+}
+
+func NewQueryBuilder() *queryBuilder {
+	return &queryBuilder{sumFields: map[string]bool{}}
+}
+
+func (qb *queryBuilder) Build() (bson.D, bson.D, *options.FindOptions) {
 	findOptions := options.Find()
-	if m.limit > 0 {
-		findOptions.SetLimit(m.limit)
+	if qb.limit > 0 {
+		findOptions.SetLimit(qb.limit)
 	}
-	if m.offset > 0 {
-		findOptions.SetSkip(m.offset)
+	if qb.offset > 0 {
+		findOptions.SetSkip(qb.offset)
 	}
-	if len(m.sort) > 0 {
-		findOptions.SetSort(m.sort)
+	if len(qb.sort) > 0 {
+		findOptions.SetSort(qb.sort)
 	}
-
-	// 執行查詢
-	cursor, err := m.collection.Find(ctx, finalFilter, findOptions)
-	if err != nil {
-		return nil, err
-	}
-	defer cursor.Close(ctx)
-
-	// 解析結果
-	var results []bson.M
-	if err = cursor.All(ctx, &results); err != nil {
-		return nil, err
-	}
-
-	return results, nil
+	return qb.filter, qb.group, findOptions
 }
 
-func (m *mongoDBWrapper) Count(ctx context.Context, collection string) (int64, error) {
-	m.collection = m.client.Database(m.database).Collection(collection)
-	count, err := m.collection.CountDocuments(ctx, m.queryFilter)
-	if err != nil {
-		return 0, err
+// func (qb *QueryBuilder) Where(field string, value interface{}, comparison ...string) *QueryBuilder {
+// 	var filter bson.E
+
+// 	compare := ""
+// 	if len(comparison) > 0 {
+// 		compare = comparison[0]
+// 	}
+
+// 	switch compare {
+// 	case "=":
+// 		filter = bson.E{Key: field, Value: value}
+// 	case ">":
+// 		filter = bson.E{Key: field, Value: bson.M{"$gt": value}}
+// 	case "<":
+// 		filter = bson.E{Key: field, Value: bson.M{"$lt": value}}
+// 	case ">=":
+// 		filter = bson.E{Key: field, Value: bson.M{"$gte": value}}
+// 	case "<=":
+// 		filter = bson.E{Key: field, Value: bson.M{"$lte": value}}
+// 	default:
+// 		filter = bson.E{Key: field, Value: value}
+// 	}
+
+// 	qb.filter = append(qb.filter, filter)
+// 	return qb
+// }
+
+func (qb *queryBuilder) Where(condition string) *queryBuilder {
+	parts := strings.Fields(condition)
+	if len(parts) != 3 {
+		return qb
 	}
-	return count, nil
-}
 
-func (m *mongoDBWrapper) Update(ctx context.Context, collection string, query interface{}, update interface{}) error {
-	//TODO implement me
-	panic("implement me")
-}
+	field := parts[0]
+	operator := parts[1]
+	var value any
+	value = parts[2]
+	if v, ok := value.(string); ok {
+		i, err := strconv.Atoi(v)
+		if strings.HasPrefix(v, `'`) && strings.HasSuffix(v, `'`) {
+			value = v[1 : len(v)-1]
+		} else if err == nil {
+			value = i
+		}
+	}
 
-func (m *mongoDBWrapper) UpdateBatch(ctx context.Context, collection string, updates []mongo.WriteModel) error {
-	m.collection = m.client.Database(m.database).Collection(collection)
-	_, err := m.collection.BulkWrite(ctx, updates)
-	return err
-}
-
-func (m *mongoDBWrapper) Delete(ctx context.Context, collection string, query interface{}) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (m *mongoDBWrapper) DeleteBatch(ctx context.Context, collection string, deletes []mongo.WriteModel) error {
-	m.collection = m.client.Database(m.database).Collection(collection)
-	_, err := m.collection.BulkWrite(ctx, deletes)
-	return err
-}
-
-func (m *mongoDBWrapper) Where(field string, comparison string, value interface{}) MongoDBWrapper {
 	var filter bson.E
-
-	switch comparison {
+	switch operator {
 	case "=":
 		filter = bson.E{Key: field, Value: value}
 	case ">":
@@ -140,39 +231,98 @@ func (m *mongoDBWrapper) Where(field string, comparison string, value interface{
 		filter = bson.E{Key: field, Value: bson.M{"$gte": value}}
 	case "<=":
 		filter = bson.E{Key: field, Value: bson.M{"$lte": value}}
-		// 更多條件...
+	default:
+		return qb
 	}
 
-	m.queryFilter = append(m.queryFilter, filter)
-	return m
+	qb.filter = append(qb.filter, filter)
+	return qb
 }
 
-func (m *mongoDBWrapper) Sort(field string, order string) MongoDBWrapper {
-	sortOrder := 1 // Ascending
-	if order == "desc" {
-		sortOrder = -1 // Descending
+// Sort default ASC, use `-` prefix as desc, example: "-age" is "age desc"
+func (qb *queryBuilder) Sort(fields ...string) *queryBuilder {
+	for _, field := range fields {
+		order := 1 // Ascending
+		if strings.HasPrefix(field, "-") {
+			order = -1 // Descending
+			field = strings.TrimPrefix(field, "-")
+		}
+		qb.sort = append(qb.sort, bson.E{Key: field, Value: order})
+	}
+	return qb
+}
+
+func (qb *queryBuilder) Limit(limit int64) *queryBuilder {
+	qb.limit = limit
+	return qb
+}
+
+func (qb *queryBuilder) Offset(offset int64) *queryBuilder {
+	qb.offset = offset
+	return qb
+}
+
+// func (qb *QueryBuilder) GroupBy(id interface{}) *QueryBuilder {
+// 	qb.group = append(qb.group, bson.E{Key: "_id", Value: id})
+// 	return qb
+// }
+
+func (qb *queryBuilder) GroupBy(fields ...string) *queryBuilder {
+	groupFields := bson.D{}
+	for _, field := range fields {
+		groupFields = append(groupFields, bson.E{Key: field, Value: "$" + field})
+	}
+	qb.group = append(qb.group, bson.E{Key: "_id", Value: groupFields})
+	return qb
+}
+
+func (qb *queryBuilder) Sum(fields ...string) *queryBuilder {
+	for _, field := range fields {
+		qb.sumFields[field] = true
+		qb.group = append(qb.group, bson.E{Key: "total_$" + field, Value: bson.D{{"$sum", field}}})
+	}
+	return qb
+}
+
+func (qb *queryBuilder) Having(condition string) *queryBuilder {
+	parts := strings.Fields(condition)
+	if len(parts) != 3 {
+		return qb
 	}
 
-	m.sort = append(m.sort, bson.E{Key: field, Value: sortOrder})
-	return m
-}
+	field := parts[0]
+	// sum field with prefix total_$
+	if _, ok := qb.sumFields[field]; ok {
+		field = "total_$" + field
+	}
+	operator := parts[1]
+	var value any
+	value = parts[2]
+	if v, ok := value.(string); ok {
+		i, err := strconv.Atoi(v)
+		if strings.HasPrefix(v, `'`) && strings.HasSuffix(v, `'`) {
+			value = v[1 : len(v)-1]
+		} else if err == nil {
+			value = i
+		}
+	}
 
-func (m *mongoDBWrapper) Limit(limit int64) MongoDBWrapper {
-	m.limit = limit
-	return m
-}
+	var filter bson.E
+	switch operator {
+	case "=":
+		filter = bson.E{Key: field, Value: value}
+	case ">":
+		filter = bson.E{Key: field, Value: bson.M{"$gt": value}}
+	case "<":
+		filter = bson.E{Key: field, Value: bson.M{"$lt": value}}
+	case ">=":
+		filter = bson.E{Key: field, Value: bson.M{"$gte": value}}
+	case "<=":
+		filter = bson.E{Key: field, Value: bson.M{"$lte": value}}
+	default:
+		return qb
+	}
 
-func (m *mongoDBWrapper) Offset(offset int64) MongoDBWrapper {
-	m.offset = offset
-	return m
-}
-
-func (m *mongoDBWrapper) GroupBy(field string) MongoDBWrapper {
-	m.groupBy = bson.D{{Key: "$group", Value: bson.D{{Key: "_id", Value: "$" + field}}}}
-	return m
-}
-
-func (m *mongoDBWrapper) Having(condition bson.M) MongoDBWrapper {
-	m.groupBy = append(m.groupBy, bson.E{Key: "$match", Value: condition})
-	return m
+	qb.having = append(qb.having, filter)
+	return qb
 }
